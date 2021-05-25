a5ef1883d15d9582f5e45bed3c64edfa8b0dd2ab
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-05-25 15:57:06 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, string) {
+   const newArray = array.filter(name => name.toLowerCase() === string.toLowerCase())
+   return newArray
+}
+
+function fuzzyMatch(array, letter) {
+    const newArray = array.filter(name => name.charAt(0) === letter.charAt(0))
+    return newArray
+ }
+
+ function matchName(array, string) {
+    const match = array.filter(driver => driver.name === string)
+    return match
+ }
